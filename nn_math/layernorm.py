a5182e8da3b1a02af0c5b66bb68bfb23d6510a0b
Nick <Nickvuletich23@gmail.com>
2026-03-17 16:26:52 -0400

    fixing wording

--- nn_math/layernorm.py
+++ nn_math/layernorm.py
@@ -4,7 +4,7 @@
 
 import numpy as np
 
-def layer_norm(x, gamma, beta, esp=1e-5):
+def layer_norm(x, gamma, beta, eps=1e-5):
     """
     Layer Normalization
 
@@ -32,6 +32,6 @@ def layer_norm(x, gamma, beta, esp=1e-5):
     mean = np.mean(x, axis=-1, keepdims=True)
     var = np.var(x, axis=-1, keepdims=True)
 
-    x_norm = (x - mean) / np.sqrt(var + esp)
+    x_norm = (x - mean) / np.sqrt(var + eps)
 
     return gamma * x_norm + beta
