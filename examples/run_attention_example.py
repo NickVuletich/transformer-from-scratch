99844832c05f90020e1a8d63e9c40b355932988a
Nick <Nickvuletich23@gmail.com>
2026-03-27 23:44:01 -0400

    finished the attention example

--- examples/run_attention_example.py
+++ examples/run_attention_example.py
@@ -20,3 +20,41 @@ token_ids = [vocab[word] for word in sentence]
 
 #----------Embedding----------
 
+vocab_size = len(vocab)
+embed_dim = 8
+
+# Random Embedding matrix
+E = np.random.randn(vocab_size, embed_dim)
+
+X = np.array([E[token] for token in token_ids])
+
+#----------Init Weights----------
+hidden = 16
+
+Wq = np.random.randn(embed_dim,embed_dim)
+Wk = np.random.randn(embed_dim,embed_dim)
+Wv = np.random.randn(embed_dim,embed_dim)
+
+W1 = np.random.randn(embed_dim,hidden)
+b1 = np.random.randn(hidden)
+
+W2 = np.random.randn(hidden,embed_dim)
+b2 = np.random.randn(embed_dim)
+
+gamma1 = np.ones(embed_dim)
+beta1 = np.zeros(embed_dim)
+
+gamma2 = np.ones(embed_dim)
+beta2 = np.zeros(embed_dim)
+
+#----------transformer----------
+output = transformer_block_prenorm(X, Wq, Wk, Wv, W1, b1, W2, b2, gamma1, beta1, gamma2, beta2)
+
+#----------Printing----------
+print("Sentence: ", sentence)
+print("Token IDs: ", token_ids)
+print("Input Shape: ", X.shape)
+print("Output Shape", output.shape)
+print()
+print("Output Vectors (first 2 tokens): ")
+print(output[:2])
